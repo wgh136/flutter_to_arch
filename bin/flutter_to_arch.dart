9fedb9204babef3171883e6017de29fdccd56bb7
nyne <nyne19710@proton.me>
2024-09-03 23:31:51 +0800

    initial commit

---
+++ bin/flutter_to_arch.dart
@@ -0,0 +1,91 @@
+import 'package:flutter_to_arch/flutter_to_arch.dart' as flutter_to_arch;
+import 'package:io/io.dart';
+import 'dart:io';
+
+import 'package:yaml/yaml.dart';
+
+void main(List<String> arguments) async {
+  var file = File("pubspec.yaml");
+  if (!file.existsSync()) {
+    print("pubspec.yaml not found");
+    exit(1);
+  }
+  var doc = loadYaml(file.readAsStringSync());
+  if (doc['flutter_to_arch'] == null) {
+    print("config not found");
+    exit(1);
+  }
+  var pkgName = doc['name'];
+  if (pkgName == null) {
+    print("name is required");
+    exit(1);
+  }
+  var description = doc['description'];
+  if (description == null) {
+    print("description is required");
+    exit(1);
+  }
+  var name = doc['flutter_to_arch']['name'];
+  if (name == null) {
+    print("name is required");
+    exit(1);
+  }
+  var icon = doc['flutter_to_arch']['icon'];
+  if (icon == null) {
+    print("icon is required");
+    exit(1);
+  }
+  var categories = doc['flutter_to_arch']['categories'];
+  if (categories == null) {
+    print("categories is required");
+    exit(1);
+  }
+  var keywords = doc['flutter_to_arch']['keywords'];
+  if (keywords == null) {
+    print("keywords is required");
+    exit(1);
+  }
+  var version = (doc['version'] as String).split('+').first;
+  var buildNumber = (doc['version'] as String).split('+')[1];
+  var depends =
+      (doc['flutter_to_arch']['depends'] as List?)?.map((e) => "'$e'").join(' ');
+  if (depends == null) {
+    print("depends is required");
+    exit(1);
+  }
+  var url = doc['flutter_to_arch']['url'];
+  if (url == null) {
+    print("url is required");
+    exit(1);
+  }
+
+  var desktopFileContent = flutter_to_arch.generateDesktopFile(
+      version, name, pkgName, description, categories, keywords);
+  var pkgBuildContent = flutter_to_arch.generatePkgBuild(
+      pkgName, description, version, buildNumber, url, depends);
+  Directory.current = Directory('${Directory.current.path}/build/linux');
+  if (Directory("app").existsSync()) {
+    Directory("app").deleteSync(recursive: true);
+  }
+  Directory("app").createSync(recursive: true);
+  copyPathSync("x64/release/bundle/", "app");
+  File("app/icon.png")
+      .writeAsBytesSync(File("../../$icon").readAsBytesSync());
+  File("app/app.desktop").writeAsStringSync(desktopFileContent);
+  Process.runSync(
+      'tar', ['-czvf', 'app.tar.gz', 'app']);
+  Directory("app").deleteSync(recursive: true);
+  if (Directory('arch').existsSync()) {
+    Directory('arch').deleteSync(recursive: true);
+  }
+  Directory('arch').createSync(recursive: true);
+  File('arch/PKGBUILD').writeAsStringSync(pkgBuildContent);
+  File("app.tar.gz").renameSync("arch/app.tar.gz");
+  Directory.current = Directory('${Directory.current.path}/arch');
+  var result = Process.runSync('makepkg', ['-s']);
+  if(result.exitCode != 0) {
+    print("makepkg finished with exit code ${result.exitCode}");
+    exit(1);
+  }
+  print("\x1b[32mbuild/linux/arch/$pkgName-$version-$buildNumber-x86_64.pkg.tar.zst\x1b[0m");
+}
---
+++ lib/flutter_to_arch.dart
@@ -0,0 +1,41 @@
+String generateDesktopFile(String version, String name, String pkgName, String description, String categories, String keywords) {
+  return '''
+[Desktop Entry]
+Version=$version
+Name=$name
+GenericName=$name
+Comment=$description
+Terminal=false
+Type=Application
+Categories=$categories
+Exec=/usr/bin/${pkgName}_pkg/$pkgName
+Keywords=$keywords
+Icon=/usr/share/icons/hicolor/64x64/apps/$pkgName.png
+'''.trim();
+}
+
+String generatePkgBuild(String pkgName, String description, String version, String buildNumber, String url, String depends) {
+  return '''
+pkgname=$pkgName
+pkgver=$version
+pkgrel=$buildNumber
+pkgdesc="$description"
+arch=('x86_64')
+source=("app.tar.gz")
+md5sums=('SKIP')
+url="$url"
+depends=($depends)
+
+package() {
+    cd "\$srcdir"
+    install -Dm755 app/pica_comic "\$pkgdir/usr/bin/${pkgName}_pkg/pica_comic"
+    install -d "\$pkgdir/usr/bin/${pkgName}_pkg/lib"
+    cp -r app/lib/* "\$pkgdir/usr/bin/${pkgName}_pkg/lib/"
+    install -d "\$pkgdir/usr/bin/${pkgName}_pkg/data"
+    cp -r app/data/* "\$pkgdir/usr/bin/${pkgName}_pkg/data/"
+    install -Dm644 app/icon.png "\$pkgdir/usr/share/icons/hicolor/64x64/apps/$pkgName.png"
+    install -Dm644 app/app.desktop "\$pkgdir/usr/share/applications/\$pkgname.desktop"
+    ln -s "\$pkgdir/usr/bin/${pkgName}_pkg/$pkgName" "\$pkgdir/usr/bin/$pkgName"
+}
+'''.trim();
+}
